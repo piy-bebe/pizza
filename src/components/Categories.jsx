f6d94d7aff3ae123727440daf25ac4ec2bd5c95c
piy-bebe <piy.bebe@gmail.com>
2022-10-20 17:33:25 +0300

    create category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const Categories = () => {
+  const [activeCategory, setActiveCategory] = React.useState(0)
+  const categories = ['Все', 'Мясные', 'Вегетариансике', 'Гриль', 'Острые', 'Закрытые']
+
   return (
     <div class="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((category, index) => (
+          <li onClick={() => setActiveCategory(index)} className={activeCategory === index && 'active'}>
+            {category}
+          </li>
+        ))}
       </ul>
     </div>
   )
